119d8922356b41d0c2d2a4fbfc943a8d88e592fa
Александр Утбанов <utbanovag@it2g.ru>
2021-07-24 19:13:54 +0300

    fix onBoarding

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -5,9 +5,30 @@ import {Tabs, Tab, TabIcon} from "../core/components";
 import { Onboarding } from "../screens/Onboarding/Onboarding";
 import { RoomsNavigator } from "./RoomsNavigator";
 import {STORAGE_KEYS} from "../shared/constants";
+import { View } from "react-native";
+
+const TabNavigation = () => (
+    <Tabs>
+        <Tab 
+            name="full_list"
+            icon={<TabIcon.Default iconSrc={require('../assets/icon-24-list.png')} title="Комнаты" />}
+            iconActive={<TabIcon.Active iconSrc={require('../assets/icon-24-list-active.png')} title="Комнаты" />}
+        >
+            <RoomsNavigator />
+        </Tab>
+
+        <Tab 
+            name="chosen_list"
+            icon={<TabIcon.Default iconSrc={require('../assets/icon-24-like.png')} title="Понравилось" />}
+            iconActive={<TabIcon.Active iconSrc={require('../assets/icon-24-like-active.png')} title="Понравилось" />}
+        >
+            <RoomsNavigator liked />
+        </Tab>
+    </Tabs>
+)
 
 export const Navigation = () => {
-    const [started, setStarted] = useState(false);
+    const [screen, setScreen] = useState(null);
     const watchedOnboardingStorage = useAsyncStorage(STORAGE_KEYS.watchedOnboarding);
 
     const getWatchedOnboarding = async () => {
@@ -15,7 +36,11 @@ export const Navigation = () => {
             const data = await watchedOnboardingStorage.getItem();
             const isWatched = JSON.parse(data);
 
-            setStarted(isWatched);
+            if (isWatched) {
+                setScreen(<TabNavigation />);
+            } else {
+                setScreen(<Onboarding onStart={() => setScreen(<TabNavigation />)} />);
+            }
         } catch(error) {
             console.log(error);
         }
@@ -25,27 +50,5 @@ export const Navigation = () => {
         getWatchedOnboarding();
     }, [])
 
-    const handleStart = useCallback(() => {
-        setStarted(true);
-    }, [])
-
-    return started ? (
-        <Tabs>
-            <Tab 
-                name="full_list"
-                icon={<TabIcon.Default iconSrc={require('../assets/icon-24-list.png')} title="Комнаты" />}
-                iconActive={<TabIcon.Active iconSrc={require('../assets/icon-24-list-active.png')} title="Комнаты" />}
-            >
-                <RoomsNavigator />
-            </Tab>
-
-            <Tab 
-                name="chosen_list"
-                icon={<TabIcon.Default iconSrc={require('../assets/icon-24-like.png')} title="Понравилось" />}
-                iconActive={<TabIcon.Active iconSrc={require('../assets/icon-24-like-active.png')} title="Понравилось" />}
-            >
-                <RoomsNavigator liked />
-            </Tab>
-        </Tabs>
-    ) : <Onboarding onStart={handleStart}/>
+    return !!screen ? screen : <View style={{flex: 1, backgroundColor: '#050506'}} />
 }
