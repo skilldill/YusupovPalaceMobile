874189c12263423a471d54d2810feaa85cb09582
Александр Утбанов <utbanovag@it2g.ru>
2021-08-01 02:09:08 +0300

    create control of audioPlayer

--- screens/AudioPlayer/AudioPlayer.js
+++ screens/AudioPlayer/AudioPlayer.js
@@ -1,18 +1,43 @@
 import React, { useEffect } from "react";
-import {View, Modal} from "react-native";
+import {View, Text, Modal, TouchableOpacity, Image} from "react-native";
 
 import {audioPlayerStyle} from "./style";
 import {useAudioPlayer} from "../../core/context";
 
 export const AudioPlayer = ({ children }) => {
-    const {setAudio, plaing} = useAudioPlayer();
+    const audioPlayer = useAudioPlayer();
+
+    const togglePlayer = () => {
+        if (audioPlayer.pausing) {
+            audioPlayer.playAudio();
+        } else {
+            audioPlayer.pauseAudio();
+        }
+    }
 
     return (
         <View style={audioPlayerStyle.container}>
             {children}
-            {plaing && (
+            {audioPlayer.plaing && (
                 <View style={audioPlayerStyle.audioControl}>
-
+                    {!!audioPlayer.roomData && (
+                        <View style={audioPlayerStyle.controlInfo}>
+                            <Image style={audioPlayerStyle.controPreview} source={{ uri: audioPlayer.roomData.preview }} />
+                            <View>
+                                <Text style={audioPlayerStyle.controlTitle}>{audioPlayer.roomData.name}</Text>
+                                <Text style={audioPlayerStyle.controlSubtitle}>Команта № {audioPlayer.roomData.id}</Text>
+                            </View>
+                        </View>
+                    )}
+                    <TouchableOpacity 
+                        style={audioPlayerStyle.controlPlayButton}
+                        onPress={togglePlayer}
+                    >
+                        <Image 
+                            style={audioPlayerStyle.controlPlayButtonIcon}
+                            source={audioPlayer.pausing ? require('../../assets/icon-audio-start.png') : require('../../assets/icon-audio-stop.png')} 
+                        />
+                    </TouchableOpacity>
                 </View>
             )}
         </View>
--- screens/AudioPlayer/style.js
+++ screens/AudioPlayer/style.js
@@ -3,17 +3,55 @@ import {StyleSheet} from "react-native";
 export const audioPlayerStyle = StyleSheet.create({
     container: {
         flex: 1,
-        // position: 'relative',
     },
 
     audioControl: {
-        // position: 'absolute',
-        // bottom: 0,
-        // left: 0,
-        // right: 0,
         height: 64,
         backgroundColor: '#1A1A1F',
         borderBottomColor: '#050506',
-        borderBottomWidth: 1
+        borderBottomWidth: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 16
+    },
+
+    controlInfo: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+
+    controPreview: {
+        width: 40,
+        height: 40,
+        borderRadius: 4,
+        marginRight: 16
+    },  
+
+    controlPlayButton: {
+        width: 32,
+        height: 32,
+        backgroundColor: '#fff',
+        borderRadius: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    controlPlayButtonIcon: {
+        transform: [{scale: 0.66}]
+    },
+
+    controlTitle: {
+        color: '#F8F9FA',
+        fontSize: 16,
+        fontWeight: '600',
+        lineHeight: 18,
+        marginBottom: 4
+    },
+
+    controlSubtitle: {
+        color: '#868E96',
+        fontSize: 14,
+        lineHeight: 16
     }
 })
